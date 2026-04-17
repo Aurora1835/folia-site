b2887f07ec1fd6642e3e8087be45c80468065a27
Aurora1835 <jessrosesilberman@gmail.com>
2026-04-17 05:16:02 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = 'folia-v1';
+const CACHE = 'folia-v2';
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll([
       '/',
@@ -10,8 +11,17 @@ self.addEventListener('install', e => {
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
